ed073534cbd19296eb864a1fe8e8fd753e1b738e
Garrett Luu <garrettluu131@gmail.com>
2018-11-03 21:31:10 -0700

    basic hello world test

--- main.js
+++ main.js
@@ -2,26 +2,4 @@ var vimMode = 0;
 
 console.log("Hello World!");
 
-docs.keyboard.startBlockingKeyboard();
-
-// docs.keyboard.handleKeyDown = function (e) {
-//    let keyPressed = e.key;
-//    switch(keyPressed) {
-//        case 'Esc':
-//            if (vimMode !== 0) {
-//                vimMode = 0;
-//                docs.keyboard.startBlockingKeyboard();
-//            }
-//            break;
-//        case 'i':
-//            if (vimMode === 0) {
-//                vimMode = 1;
-//                docs.keyboard.stopBlockingKeyboard();
-//            }
-//            break;
-//        case 'h':
-//
-//            break;
-//    }
-// };
 
