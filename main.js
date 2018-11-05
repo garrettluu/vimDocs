2df6c4d0fba95a4884df5666118a4b801d343771
Garrett Luu <garrettluu131@gmail.com>
2018-11-05 02:28:46 -0800

    basic event handling test

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var vimMode = 0;
 
 console.log("Hello World!");
+handleKeyboard = function(e) {
+    var key = e.key;
 
-
+    if (key === "a") {
+        console.log("Pressed A!");
+    }
+};
+$(".docs-texteventtarget-iframe").contents().find("[contenteditable=\"true\"]").on("keydown", handleKeyboard);
